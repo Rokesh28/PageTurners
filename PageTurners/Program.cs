bc6ea1fa1734ab9120e5efb96a704f2d6a1a9fac
Rokesh Prakash <rokeshprakash@Rokeshs-MacBook-Pro.local>
2024-06-15 22:22:12 -0400

    Bootswatch Update

--- PageTurners/Program.cs
+++ PageTurners/Program.cs
@@ -26,7 +26,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{area:Admin}{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Admin}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
