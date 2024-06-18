ec477f83c87345d9237c61c880b5a56065b0b5e7
Rokesh Prakash <rokeshprakash@Rokeshs-MacBook-Pro.local>
2024-06-18 16:07:03 -0400

    Before Identity State

--- PageTurners/Areas/Customer/Controllers/HomeController.cs
+++ PageTurners/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using PageTurners.DataAccess.Repository;
+using PageTurners.DataAccess.Repository.IRepository;
 using PageTurners.Models;
 
 namespace PageTurners.Areas.Customer.Controllers;
@@ -7,15 +9,29 @@ namespace PageTurners.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ProductImages");
+        return View(productList);
+    }
+
+    public IActionResult Details(int productId)
+    {
+        //ShoppingCart cart = new()
+        //{
+        Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category,ProductImages");
+        //    Count = 1,
+        //    ProductId = productId
+        //};
+        return View(product);
     }
 
     public IActionResult Privacy()
