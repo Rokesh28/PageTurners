0eeaf1c4d8b0681da26170d30f93ead7f71d6b81
Rokesh Prakash <rokeshprakash@Rokeshs-MacBook-Pro.local>
2024-07-08 01:54:48 -0400

    DBinitializer

--- PageTurners.DataAccess/DbInitializer/DbInitializer.cs
+++ PageTurners.DataAccess/DbInitializer/DbInitializer.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PageTurners.DataAccess.Data;
 using PageTurners.Models;
 using PageTurners.Utility;
+using System;
+using System.Linq;
 
 namespace PageTurners.DataAccess.DbInitializer
 {
@@ -23,53 +24,71 @@ namespace PageTurners.DataAccess.DbInitializer
             _db = db;
         }
 
-
         public void Initialize()
         {
-
-
-            //migrations if they are not applied
+            // Migrate the database
             try
             {
-                if (_db.Database.GetPendingMigrations().Count() > 0)
+                if (_db.Database.GetPendingMigrations().Any())
                 {
                     _db.Database.Migrate();
                 }
             }
-            catch (Exception ex) { }
-
-
-
-            //create roles if they are not created
-            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            catch (Exception ex)
             {
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
-
-
-                //if roles are not created, then we will create admin user as well
-                _userManager.CreateAsync(new ApplicationUser
-                {
-                    UserName = "Rokesh",
-                    Email = "rokesh2897@gmail.com",
-                    Name = "Rokesh Prakash",
-                    PhoneNumber = "9898945345",
-                    StreetAddress = "70 lake groove",
-                    State = "NY",
-                    PostalCode = "23422",
-                    City = "Stony Brook"
-                }, "Admin@789").GetAwaiter().GetResult();
-
-
-                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "rokesh2897@gmail.com");
-                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
-
+                // Log the exception
+                Console.WriteLine($"An error occurred while migrating the database: {ex.Message}");
+                throw;
             }
 
-            return;
+            // Create roles if they are not created
+            try
+            {
+                if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+                {
+                    _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                    _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                    _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                    _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+
+                    // Create admin user
+                    var adminUser = new ApplicationUser
+                    {
+                        UserName = "Rokesh",
+                        Email = "rokesh2897@gmail.com",
+                        Name = "Rokesh Prakash",
+                        PhoneNumber = "9898945345",
+                        StreetAddress = "70 lake groove",
+                        State = "NY",
+                        PostalCode = "23422",
+                        City = "Stony Brook"
+                    };
+                    var result = _userManager.CreateAsync(adminUser, "Admin@789").GetAwaiter().GetResult();
+
+                    if (result.Succeeded)
+                    {
+                        var user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "rokesh2897@gmail.com");
+                        if (user != null)
+                        {
+                            _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+                        }
+                    }
+                    else
+                    {
+                        // Log user creation errors
+                        foreach (var error in result.Errors)
+                        {
+                            Console.WriteLine($"Error creating admin user: {error.Description}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Console.WriteLine($"An error occurred while initializing roles and users: {ex.Message}");
+                throw;
+            }
         }
     }
 }
-
