dbab792f09159ec230e3de7eb94d91774805b68a
Rokesh Prakash <rokeshprakash@Rokeshs-MacBook-Pro.local>
2024-07-08 20:33:43 -0400

    Manage User

---
+++ PageTurners.Models/ViewModels/RoleManagmentVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PageTurners.Models.ViewModels
+{
+	public class RoleManagmentVM
+	{
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        public IEnumerable<SelectListItem> CompanyList { get; set; }
+    }
+}
+
